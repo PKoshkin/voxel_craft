cbbee9b32365e95d357c7f43dd29db7bbfd947a4
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-23 20:40:05 +0300

    Getting shaders folder from command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #[macro_use]
 extern crate glium;
+
 mod game_application;
 use game_application::GameApplication;
 
+use std::env;
+
 
 fn main() {
-    let mut game = GameApplication::new("shaders");
-    game.start_loop();
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        let mut game = GameApplication::new(".");
+        game.start_loop();
+    } else if args.len() == 2 {
+        let mut game = GameApplication::new(&args[1]);
+        game.start_loop();
+
+    } else {
+        panic!("Wrong arguments number");
+    }
 }
