485f43c6b83e3996d134d85728080f9cd3cb3a4b
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-26 01:32:45 +0300

    Trying to draw something. Shaders does not compile.

--- src/game_application/mod.rs
+++ src/game_application/mod.rs
@@ -13,34 +13,34 @@ use self::model::Model;
 mod draw_params;
 mod vertex;
 mod map;
+mod camera;
 
 
-pub struct GameApplication {
-    events_loop: glutin::EventsLoop,
-    view: View,
-    model: Model,
+pub struct GameApplication<'time> {
+    directory: &'time str
 }
 
 
-impl GameApplication {
-    pub fn new(directory: &str) -> GameApplication {
-        let events_loop = glutin::EventsLoop::new();
-        let view = View::new(&events_loop);
-        let model = Model::new();
+impl<'time> GameApplication<'time> {
+    pub fn new(directory: &'time str) -> GameApplication<'time> {
         GameApplication{
-            events_loop: events_loop,
-            view: view,
-            model: model
+            directory: directory
         }
     }
 
     pub fn start_loop(&mut self) {
         let mut closed = false;
-        let controller = Controller::new(&self.model);
+
+        let mut events_loop = glutin::EventsLoop::new();
+        let mut view = View::new(&events_loop, self.directory);
+        let mut model = Model::new();
+        let controller = Controller::new(&model);
+
         while !closed {
-            self.events_loop.poll_events(|event| {
+            events_loop.poll_events(|event| {
                 if let glutin::Event::WindowEvent{event, ..} = event {
                     controller.handle_event(&event);
+                    view.camera.handle_event(&event);
                     if let glutin::WindowEvent::KeyboardInput{input, ..} = event {
                         if let Some(key) = input.virtual_keycode {
                             if let glutin::VirtualKeyCode::Escape = key {
@@ -50,8 +50,9 @@ impl GameApplication {
                     }
                 }
             });
-            let draw_params = self.model.get_draw_params();
-            self.view.draw(draw_params);
+            view.camera.update();
+            let draw_params = model.get_draw_params();
+            view.draw(draw_params);
         }
     }
 }
--- src/game_application/view.rs
+++ src/game_application/view.rs
@@ -1,26 +1,107 @@
 use glium::{self, Surface};
 use game_application::glutin;
 use game_application::draw_params::DrawParams;
+use game_application::camera::Camera;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+extern crate image;
+use std::io::Cursor;
 
 
 pub struct View {
-    display: glium::Display
+    pub camera: Camera,
+    display: glium::Display,
+    program: glium::Program,
+    texture: glium::texture::SrgbTexture2d,
+    normal_map: glium::texture::Texture2d
+}
+
+
+fn init_shaders(display: &glium::Display, directory: &str) -> glium::Program {
+    let mut vertex_shader_file = File::open(directory.to_string() + "/src/shaders/shader.vert").unwrap();
+    let mut fragment_shader_file = File::open(directory.to_string() + "/src/shaders/shader.frag").unwrap();
+    let mut vertex_shader_src = String::new();
+    let mut fragment_shader_src = String::new();
+    vertex_shader_file.read_to_string(&mut vertex_shader_src).expect("Failed to read vertex shader file!");
+    fragment_shader_file.read_to_string(&mut fragment_shader_src).expect("Failed to read fragment shader file!");
+    glium::Program::from_source(display, &vertex_shader_src, &fragment_shader_src, None).unwrap()
+}
+
+
+fn init_textures(display: &glium::Display) -> (glium::texture::SrgbTexture2d, glium::texture::Texture2d) {
+    let image = image::load(Cursor::new(&include_bytes!("../../images/grass_texture.jpg")[..]), image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let texture = glium::texture::SrgbTexture2d::new(display, image).unwrap();
+
+    let image = image::load(Cursor::new(&include_bytes!("../../images/grass_normal_map.png")[..]), image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let normal_map = glium::texture::Texture2d::new(display, image).unwrap();
+
+    (texture, normal_map)
 }
 
 
 impl View {
-    pub fn new(events_loop: &glutin::EventsLoop) -> View  {
+    pub fn new(events_loop: &glutin::EventsLoop, directory: &str) -> View  {
+        println!("ECHO 1");
         let window = glutin::WindowBuilder::new().with_decorations(false).with_fullscreen(Some(events_loop.get_primary_monitor()));
         let context = glutin::ContextBuilder::new().with_depth_buffer(24);
         let display = glium::Display::new(window, context, &events_loop).unwrap();
+        println!("ECHO 2");
+
+        // shaders init
+        let program = init_shaders(&display, directory);
+        println!("ECHO 3");
+
+        // textires init
+        let (texture, normal_map) = init_textures(&display);
+        println!("ECHO 4");
+
+        // camera init
+        let (width, height) = display.get_framebuffer_dimensions();
+        let aspect_ratio = width as f32 / height as f32;
+        let camera = Camera::new(aspect_ratio, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0));
+        println!("ECHO 1");
         View{
-            display: display    
+            display: display,
+            program: program,
+            camera: camera,
+            texture: texture,
+            normal_map: normal_map
         }
     }
 
+
     pub fn draw(&self, draw_params: DrawParams) {
+        let shape = glium::vertex::VertexBuffer::new(&self.display, &draw_params.shape).unwrap();
+        let indices = glium::IndexBuffer::new(&self.display, glium::index::PrimitiveType::TrianglesList, &draw_params.indices).unwrap();
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockWise,
+            .. Default::default()
+        };
         let mut target = self.display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        let view = self.camera.get_view();
+        let perspective = self.camera.get_perspective();
+        let light = [1.4, 0.4, -0.7f32];
+        let model = [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32]
+        ];
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+        target.draw(&shape, &indices, &self.program,
+                    &uniform!{model: model, view: view, perspective: perspective, diffuse_tex: &self.texture, normal_tex: &self.normal_map, u_light: light},
+                    &params).unwrap();
         target.finish().unwrap();
     }
 }
