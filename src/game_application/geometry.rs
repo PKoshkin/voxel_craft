a9e769efad2fd0cb8a7e5f1f9e8481c10513183e
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-30 09:22:18 +0300

    Right normals calculating

--- src/game_application/geometry.rs
+++ src/game_application/geometry.rs
@@ -1,8 +1,6 @@
-use game_application::cgmath::{Vector3, dot};
+use game_application::cgmath::{Vector3, dot, ElementWise};
 
 pub fn normalize(vector: &mut Vector3<f32>) {
-    let length = dot(*vector, *vector);
-    vector.x /= length;
-    vector.y /= length;
-    vector.z /= length;
+    let length = dot(*vector, *vector).sqrt();
+    vector.div_assign_element_wise(length);
 }
